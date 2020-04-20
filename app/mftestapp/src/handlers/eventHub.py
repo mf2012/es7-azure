958192b457ae04591540a0239069ff235195126c
Marek Fengler <mf2012a@qbit.ga>
2020-04-20 09:26:44 -0600

    Removed test app

--- app/mftestapp/src/handlers/eventHub.py
+++
@@ -1,9 +0,0 @@
-import logging
-import azure.functions as func
-
-
-def main(eventHubMessages: func.EventHubEvent):
-    logging.info('Function triggered to process a message: ', event.get_body())
-    logging.info('  EnqueuedTimeUtc =', event.enqueued_time)
-    logging.info('  SequenceNumber =', event.sequence_number)
-    logging.info('  Offset =', event.offset)
